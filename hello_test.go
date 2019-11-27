fdf0c9faa8713c557d18ab635b48824d197b0933
yunxiuhe <yunxiuhe@100tal.com>
2019-11-27 21:35:16 +0800

    学习了迭代 基准测试 事例文档

--- hello_test.go
+++ hello_test.go
@@ -7,6 +7,6 @@ func TestHello(t *testing.T) {
 	want := "Hello, World"
 
 	if got != want {
-		t.Errorf("got '%q' want '%q' ", got, want)
+		t.Errorf("got %q want %q ", got, want)
 	}
 }
--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,14 +1,23 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestAdder(t *testing.T)  {
+func TestAdder(t *testing.T) {
 	t.Run("testing adder", func(t *testing.T) {
-		sum := Add(2,2)
+		sum := Add(2, 2)
 		expected := 4
 
 		if sum != expected {
 			t.Errorf("expected '%d' but got '%d' ", expected, sum)
 		}
 	})
+}
+
+func ExampleAdd() {
+	sum := Add(3, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
