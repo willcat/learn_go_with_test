bfbccd0988c7bab093bd317e804bbf0d032d42fe
yunxiuhe <yunxiuhe@100tal.com>
2019-12-02 12:38:32 +0800

    finish method learning

--- math/math.go
+++ math/math.go
@@ -1,14 +1,24 @@
 package math
 
+import "math"
+
 type Rectangle struct {
 	Height float64
 	Width  float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Height + rectangle.Width)
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return c.Radius * c.Radius * math.Pi
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Width)
 }
--- math/math_test.go
+++ math/math_test.go
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rect := Rectangle{10.0, 20.0}
-	got := Area(rect)
-	want := 200.0
+	t.Run("rectangles", func(t *testing.T) {
+		rect := Rectangle{10.0, 20.0}
+		got := rect.Area()
+		want := 200.0
 
-	if got != want {
-		t.Errorf("got %.2f want %2.f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %2.f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if want != got {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
 }
