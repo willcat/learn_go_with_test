19b85f07abe859221d9b09b3730d76a4bedb3d34
yunxiuhe <yunxiuhe@100tal.com>
2019-12-05 20:34:55 +0800

    给类型别名添加方法

--- pointer/wallet.go
+++ pointer/wallet.go
@@ -1,5 +1,7 @@
 package pointer
 
+import "fmt"
+
 type Bitcoin int
 
 type Wallet struct {
@@ -12,4 +14,8 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
